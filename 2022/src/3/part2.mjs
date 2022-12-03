d9ac6a0e1b5b5caee6a969dc44e287969bfd1cf1
dsf3449 <dsf190@me.com>
2022-12-03 15:41:37 -0600

    day 3

---
+++ 2022/src/3/part2.mjs
@@ -0,0 +1,32 @@
+import { getPuzzleInput } from "../utils.mjs";
+
+const UPPER_OFFSET = 38;
+const LOWER_OFFSET = 96;
+
+function getPriorityForLetter(letter) {
+  const isUpperCase = /^[A-Z]*$/.test(letter);
+  const charCode = letter.charCodeAt(0);
+
+  return isUpperCase ? charCode - UPPER_OFFSET : charCode - LOWER_OFFSET;
+}
+
+function handle(input) {
+  let sum = 0;
+
+  for (let i = 0; i < input.length; i += 3) {
+    const elfOne = input[i];
+    const elfTwo = input[i + 1];
+    const elfThree = input[i + 2];
+
+    for (const letter of elfOne.split(``).values()) {
+      if (elfTwo.includes(letter) && elfThree.includes(letter)) {
+        sum += getPriorityForLetter(letter);
+        break;
+      }
+    }
+  }
+
+  console.log(sum);
+}
+
+getPuzzleInput(3, false).then(handle);
