778d2bf490cff3b00e6de3caab636e3e8debf215
dsf3449 <dsf190@me.com>
2022-12-03 14:57:14 -0600

    day 2

--- 2022/src/utils.mjs
+++ 2022/src/utils.mjs
@@ -7,6 +7,7 @@ const __dirname = dirname(__filename);
 
 /**
  * Returns a promise which resolves into an array of the puzzle input split on newline characters.
+ * Always removes the last element of the array to account for the empty line at the end of the file.
  * @param {number} day
  * @param {boolean} useTestInput
  * @returns {Promise<string[]>}
@@ -21,5 +22,10 @@ export async function getPuzzleInput(day, useTestInput = false) {
     ),
     { encoding: `utf8` }
   );
-  return input.split(`\n`);
+
+  const res = input.split(`\n`);
+
+  // remove newline at the end of the input
+  res.splice(-1);
+  return res;
 }
