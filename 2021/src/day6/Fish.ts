f3d635acb9d4f0060971211c943f30c99ec5573c
dsf3449 <dsf190@me.com>
2022-12-03 12:00:06 -0600

    zip up 2021

--- 2021/src/day6/Fish.ts
+++
@@ -1,7 +0,0 @@
-export default class Fish {
-  daysTilReproduction: number;
-
-  constructor(inputDays: number) {
-    this.daysTilReproduction = inputDays;
-  }
-}
--- 2021/src/index.ts
+++
@@ -1,3 +0,0 @@
-import runDay from "./day15/day15";
-
-console.log(runDay(2));
