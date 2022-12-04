f590e448482f997e8f731e4a23a4dcdaf4d4fcbd
dsf3449 <dsf190@me.com>
2022-12-04 13:24:11 -0600

    day 4

---
+++ 2022/src/4/part2.mjs
@@ -0,0 +1,19 @@
+import { getPuzzleInputRel } from "../utils.mjs";
+
+function handle(input) {
+  let overlap = 0;
+
+  input.forEach((pair) => {
+    const [first, second] = pair
+      .split(`,`)
+      .map((range) => range.split(`-`).map(Number));
+
+    if (first[0] <= second[1] && first[1] >= second[0]) {
+      overlap += 1;
+    }
+  });
+
+  console.log(overlap);
+}
+
+getPuzzleInputRel(import.meta.url, false).then(handle);
