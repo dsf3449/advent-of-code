675a99f0c00324cea1779ef8cdc978d77ff5f1ec
dsf3449 <dsf190@me.com>
2023-12-01 13:13:38 -0600

    move 2022 to archive

--- 2022/src/index.mjs
+++
@@ -1,5 +0,0 @@
-import { getPuzzleInput } from "./utils.mjs";
-
-getPuzzleInput(1)
-  .then((input) => console.log(input))
-  .catch((err) => console.error(err));
--- 2022/src/template.mjs
+++
@@ -1,9 +0,0 @@
-import { getPuzzleInputRel, isImported } from "../utils.mjs";
-
-function handle(input) {
-  console.log(input);
-}
-
-if (!isImported(import.meta.url)) {
-  getPuzzleInputRel(import.meta.url, true).then(handle);
-}
