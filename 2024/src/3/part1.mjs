a74bd735a242b3ff91d00e71f59c21436a4b98ad
dsf3449 <dsf190@me.com>
2024-12-03 15:39:15 -0600

    day3

--- 2024/src/3/part1.mjs
+++ 2024/src/3/part1.mjs
@@ -1,9 +1,21 @@
 import { getPuzzleInputRel, isImported } from "../utils.mjs";
 
 function handle(input) {
-  console.log(input);
+  let sum = 0;
+  const operations = [];
+  input.forEach((line) => {
+    operations.push(...line.match(/mul\(\d+,\d+\)/gm));
+  });
+
+  operations.forEach((op) => {
+    const [first, second] = op.split(",");
+    sum +=
+      parseInt(first.replace(/\D/g, "")) * parseInt(second.replace(/\D/g, ""));
+  });
+
+  console.log(sum);
 }
 
 if (!isImported(import.meta.url)) {
-  getPuzzleInputRel(import.meta.url, true).then(handle);
+  getPuzzleInputRel(import.meta.url, false).then(handle);
 }
